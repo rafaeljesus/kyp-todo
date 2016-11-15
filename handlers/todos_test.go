d226fba9154eea897e4856a9ac7452da722c13e0
Rafael Jesus <rafaelljesus86@gmail.com>
2016-11-15 01:23:34 -0200

    Add tests for index todos

--- handlers/todos_test.go
+++ handlers/todos_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"strings"
 	"testing"
@@ -26,7 +27,7 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func TestUsersCreate(t *testing.T) {
+func TestTodosCreate(t *testing.T) {
 	response := `{"title":"buy a milk", "user_id":1}`
 	e := echo.New()
 	req, _ := http.NewRequest(echo.POST, "/v1/todos", strings.NewReader(response))
@@ -36,5 +37,22 @@ func TestUsersCreate(t *testing.T) {
 
 	if assert.NoError(t, env.TodosCreate(ctx)) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
+	}
+}
+
+func TestTodosIndex(t *testing.T) {
+	response := `{"title":"buy a milk", "user_id":1}`
+	e := echo.New()
+	q := make(url.Values)
+	q.Set("title", "buy a milk")
+	q.Set("user_id", "1")
+
+	req, _ := http.NewRequest(echo.GET, "/v1/todos?"+q.Encode(), strings.NewReader(response))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(standard.NewRequest(req, e.Logger()), standard.NewResponse(rec, e.Logger()))
+
+	if assert.NoError(t, env.TodosIndex(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
